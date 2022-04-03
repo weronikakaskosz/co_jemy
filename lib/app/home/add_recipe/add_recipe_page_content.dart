88ba75b790e151e0d1321d2d6997a66081383152
Weronika Kaśkosz <weronikakaskosz@op.pl>
2022-04-03 21:12:42 +0200

    Add Tab and TextField

--- lib/app/home/add_recipe/add_recipe_page_content.dart
+++ lib/app/home/add_recipe/add_recipe_page_content.dart
@@ -7,8 +7,100 @@ class AddRecipePageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Jaki chcesz dodać przepis?'),
+          centerTitle: true,
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Przepis na Obiad'),
+              Tab(text: 'Przepis na Ciasto'),
+            ],
+          ),
+        ),
+        body: const TabBarView(children: [
+          AddDinnerRecipePageTab(),
+          AddCakeRecipePageTab(),
+        ]),
+      ),
+    );
+  }
+}
+
+class AddDinnerRecipePageTab extends StatelessWidget {
+  const AddDinnerRecipePageTab({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Nazwa',
+          ),
+        ),
+        TextField(
+          minLines: 4,
+          maxLines: 8,
+          keyboardType: TextInputType.multiline,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Składniki',
+          ),
+        ),
+        TextField(
+          minLines: 8,
+          maxLines: 12,
+          keyboardType: TextInputType.multiline,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Przepis',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class AddCakeRecipePageTab extends StatelessWidget {
+  const AddCakeRecipePageTab({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Nazwa',
+          ),
+        ),
+        TextField(
+          minLines: 4,
+          maxLines: 8,
+          keyboardType: TextInputType.multiline,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Składniki',
+          ),
+        ),
+        TextField(
+          minLines: 8,
+          maxLines: 12,
+          keyboardType: TextInputType.multiline,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Przepis',
+          ),
+        ),
+      ],
     );
   }
 }
