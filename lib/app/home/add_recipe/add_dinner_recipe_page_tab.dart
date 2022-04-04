898f585448a0d91332684404f764790cc744e23a
Weronika Kaśkosz <weronikakaskosz@op.pl>
2022-04-04 15:49:40 +0200

    Implement adding recipes dinner and recipes cake

--- lib/app/home/add_recipe/add_dinner_recipe_page_tab.dart
+++ lib/app/home/add_recipe/add_dinner_recipe_page_tab.dart
@@ -1,45 +1,82 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddDinnerRecipePageTab extends StatelessWidget {
+class AddDinnerRecipePageTab extends StatefulWidget {
   const AddDinnerRecipePageTab({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddDinnerRecipePageTab> createState() => _AddDinnerRecipePageTabState();
+}
+
+class _AddDinnerRecipePageTabState extends State<AddDinnerRecipePageTab> {
+  var dinnerRecipeName = '';
+  var ingredientsName = '';
+  var recipeName = '';
+
+  @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
+      children: [
         Padding(
-          padding: EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(10.0),
           child: TextField(
-            decoration: InputDecoration(
+            onChanged: (newValue) {
+              setState(() {
+                dinnerRecipeName = newValue;
+              });
+            },
+            decoration: const InputDecoration(
               border: OutlineInputBorder(),
               hintText: 'Nazwa',
             ),
           ),
         ),
         Padding(
-          padding: EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(10.0),
           child: TextField(
-            minLines: 8,
-            maxLines: 8,
+            onChanged: (newValue) {
+              setState(() {
+                ingredientsName = newValue;
+              });
+            },
+            minLines: 6,
+            maxLines: 6,
             keyboardType: TextInputType.multiline,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               border: OutlineInputBorder(),
               hintText: 'Składniki',
             ),
           ),
         ),
         Padding(
-          padding: EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(10.0),
           child: TextField(
-            minLines: 11,
-            maxLines: 11,
+            onChanged: (newValue) {
+              setState(() {
+                recipeName = newValue;
+              });
+            },
+            minLines: 9,
+            maxLines: 9,
             keyboardType: TextInputType.multiline,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               border: OutlineInputBorder(),
               hintText: 'Przepis',
             ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            FirebaseFirestore.instance.collection('dinner_recipes').add({
+              'name': dinnerRecipeName,
+              'ingredients': ingredientsName,
+              'recipe': recipeName,
+            });
+          },
+          child: const Text(
+            'Zatwierdź',
           ),
         ),
       ],
