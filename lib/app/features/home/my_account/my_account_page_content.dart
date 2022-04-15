7289976b749d8e46680fc58022ac5f6eb39b219c
Weronika Kaśkosz <weronikakaskosz@op.pl>
2022-04-15 16:05:23 +0200

    Add event signOut to RootCubit

--- lib/app/features/home/my_account/my_account_page_content.dart
+++ lib/app/features/home/my_account/my_account_page_content.dart
@@ -1,5 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:co_jemy/app/cubit/root_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyAccountPageContent extends StatelessWidget {
   const MyAccountPageContent({
@@ -19,7 +20,7 @@ class MyAccountPageContent extends StatelessWidget {
           const SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
-              FirebaseAuth.instance.signOut();
+              context.read<RootCubit>().signOut();
             },
             child: const Text('Wyloguj'),
           ),
