92d0ba804491ffed354e69992d950abc053d1257
Weronika Kaśkosz <weronikakaskosz@op.pl>
2022-06-02 16:43:14 +0200

    Implement Login Cubit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,6 @@
 import 'package:co_jemy/app/cubit/root_cubit.dart';
 import 'package:co_jemy/app/features/home/home_page.dart';
-import 'package:co_jemy/app/features/login/login_page.dart';
+import 'package:co_jemy/app/features/login/page/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
---
+++ lib/app/features/login/page/login_page.dart
@@ -0,0 +1,123 @@
+import 'package:co_jemy/app/features/login/cubit/login_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({
+    Key? key,
+  }) : super(key: key);
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var errorMessage = '';
+  var isCreatingAccount = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: BlocBuilder<LoginCubit, LoginState>(
+        builder: (context, state) {
+          return Scaffold(
+            backgroundColor: const Color.fromARGB(255, 252, 239, 238),
+            body: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                        isCreatingAccount == true
+                            ? 'Zarejestruj się'
+                            : 'Zaloguj się',
+                        style: GoogleFonts.courgette(fontSize: 23)),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      controller: widget.emailController,
+                      decoration: const InputDecoration(hintText: 'E-mail'),
+                    ),
+                    TextField(
+                      controller: widget.passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(hintText: 'Hasło'),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(errorMessage),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (isCreatingAccount == true) {
+                          // rejestracja
+                          try {
+                            context.read<LoginCubit>().signUp(
+                                  widget.emailController.text,
+                                  widget.passwordController.text,
+                                );
+                          } catch (error) {
+                            setState(() {
+                              errorMessage = 'Coś poszło nie tak';
+                            });
+                          }
+                        } else {
+                          // logowanie
+                          try {
+                            context.read<LoginCubit>().signIn(
+                                  widget.emailController.text,
+                                  widget.passwordController.text,
+                                );
+                          } catch (error) {
+                            setState(() {
+                              errorMessage = 'Coś poszło nie tak';
+                            });
+                          }
+                        }
+                      },
+                      child: Text(
+                          isCreatingAccount == true
+                              ? 'Zarejestruj się'
+                              : 'Zaloguj się',
+                          style: GoogleFonts.courgette(fontSize: 18)),
+                    ),
+                    const SizedBox(height: 20),
+                    if (isCreatingAccount == false) ...[
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            isCreatingAccount = true;
+                          });
+                        },
+                        child: Text('Utwórz konto',
+                            style: GoogleFonts.courgette(fontSize: 20)),
+                      ),
+                    ],
+                    if (isCreatingAccount == true) ...[
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            isCreatingAccount = false;
+                          });
+                        },
+                        child: const Text('Masz już konto?'),
+                      ),
+                    ],
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
